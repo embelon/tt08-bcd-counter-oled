a9009c4d3e1ee76ae674243506b6935a9718a11b
embelon <78412338+embelon@users.noreply.github.com>
2024-08-07 17:00:10 +0200

    Instantiated Frequency Counter and updated info.yaml

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_frequency_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,39 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  `define clk_x       ui_in[0]
+
+  `define oled_rstn   uo_out[0]
+  `define oled_vbatn  uo_out[1]
+  `define oled_vcdn   uo_out[2]
+  `define oled_csn    uo_out[3]
+  `define oled_dc     uo_out[4]
+  `define oled_clk    uo_out[5]
+  `define oled_mosi   uo_out[6]
+
+  oled_frequency_counter counter
+  (
+    .clk_ref_in(clk),
+    .reset_in(!rst_n),
+
+    .clk_x_in(clk_x),
+
+	  // Interface to controll SSD1306 OLED Display
+	  .oled_rstn_out(oled_rstn),
+	  .oled_vbatn_out(oled_vbatn),	
+	  .oled_vcdn_out(oled_vcdn),
+	  .oled_csn_out(oled_csn),
+	  .oled_dc_out(oled_dc),
+	  .oled_clk_out(oled_clk),
+	  .oled_mosi_out(oled_mosi)
+  );
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7] = ui_in[7];
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, ui_in[6:0], 1'b0};
 
 endmodule
